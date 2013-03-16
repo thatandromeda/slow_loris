a54836a2d30b17b971de4354542d28507026b75b
Andromeda Yelton <andromeda.yelton@gmail.com>
2013-03-16 14:24:16 -0400

    more tweaks

--- settings.py
+++ settings.py
@@ -10,6 +10,18 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': join(PROJECT_DIR, 'slow_loris', 'database.db'),                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -145,17 +157,6 @@ if environ.get('LOCAL_DEV'):
     DEBUG=True
     TEMPLATE_DEBUG = DEBUG
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': join(PROJECT_DIR, 'slow_loris', 'database.db'),                      # Or path to database file if using sqlite3.
-            'USER': '',                      # Not used with sqlite3.
-            'PASSWORD': '',                  # Not used with sqlite3.
-            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-        }
-    }
-    
     STATIC_ROOT = join(PROJECT_DIR, "slow_loris", "static")
 
     # how many times a suggestion can be flagged before it's suppressed from display
