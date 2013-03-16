20afb34f9b822f181b05f6feb75ea2499ee2272a
Andromeda Yelton <andromeda.yelton@gmail.com>
2013-03-16 09:17:14 -0400

    clarify communication on submit page

--- core/forms.py
+++ core/forms.py
@@ -3,6 +3,9 @@ from django.db import models
 from core.models import Suggestion
 
 class SuggestionForm(forms.ModelForm):
+    text = forms.CharField(label='How about you...')
+    link = forms.URLField(label='Link for next steps')
+    
     class Meta:
         model = Suggestion
         exclude = 'flag'
