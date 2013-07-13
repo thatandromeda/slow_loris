b941a927602e9d7835f668577a97bcc18ec6fef7
Andromeda Yelton <andromeda.yelton@gmail.com>
2013-07-13 11:29:51 -0400

    i am tired of spammers

--- core/forms.py
+++ core/forms.py
@@ -5,7 +5,14 @@ from core.models import Suggestion
 class SuggestionForm(forms.ModelForm):
     text = forms.CharField(label='How about you...')
     link = forms.URLField(label='Link for next steps')
+    captcha = forms.CharField(label="Prove you're not a spammer! What's the 2013 location of LITA Forum?")
     
     class Meta:
         model = Suggestion
         exclude = 'flag'
+        
+    def clean_captcha(self):
+        data = self.cleaned_data['captcha'].lower()
+        if 'louisville' not in data:
+            raise forms.ValidationError("Whoops, that's not where Forum is.")
+        
